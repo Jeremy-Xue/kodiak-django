2471d124cf7fc41cc14a7d030e9a2cd7c9cf56ac
Matt Liu <mcliu@andrew.cmu.edu>
2019-03-27 23:21:23 -0400

    Serializers and API setup

---
+++ kodiak_activity_backend/backend/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from backend.models import *
+
+class DaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Day
+        fields = '__all__'
+
+class ParentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parent
+        fields = '__all__'
+
+class ChildSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Child
+        fields = '__all__'
+
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = '__all__'
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = '__all__'
+
--- kodiak_activity_backend/backend/views.py
+++ kodiak_activity_backend/backend/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from backend.models import *
+from backend.serializers import *
+from rest_framework import generics
+
+class BackendListCreate(generics.ListCreateAPIView):
+    queryset = Child.objects.all()
+    serializer_class = ChildSerializer
