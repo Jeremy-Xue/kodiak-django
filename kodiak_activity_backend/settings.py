5fa11f82cae285c77d69f135cfbf7b16e5e65906
Roman Kaufman <roman.a.kaufman@gmail.com>
2019-04-03 18:53:48 -0400

    secret key change

--- kodiak_activity_backend/settings.py
+++ kodiak_activity_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 's!@8im0#2hl_-uay^6q^&16rawktc5an^=w#=(h!(11m6blryf'
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 's!@8im0#2hl_-uay^6q^&16rawktc5an^=w#=(h!(11m6blryf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # to set this to true, you can use `export DJANGO_DEBUG=False` from cmd line
