a580615aec446edbcca720f9a177fe40ae267d17
Jeremy Xue <jeremyxue@gmail.com>
2019-04-22 20:14:40 -0400

    Merging matt and jeremy code

--- backend/views.py
+++ backend/views.py
@@ -10,6 +10,8 @@ from rest_framework.response import Response
 import datetime
 from django.conf import settings
 from django.core.mail import send_mail
+import random
+import string
 
 # class BackendListCreate(generics.ListCreateAPIView):
 #     queryset = Child.objects.all()
@@ -31,11 +33,16 @@ class EnrollmentRUD(generics.RetrieveUpdateDestroyAPIView):
     queryset = Enrollment.objects.all()
     serializer_class = EnrollmentSerializer
 
-@api_view(["POST"])
-def login(request):
-    login_info = request.data
-    login_info["status"]="authorized"
-    return Response(login_info, status=status.HTTP_201_CREATED)
+
+def generate_token(e_id):
+    parent_token_info = dict()
+    parent_token_info['enrollment'] = e_id
+    parent_token_info["token"] = ''.join([random.choice(string.ascii_letters) for i in range(64)])
+    parent_serializer = ParentTokenSerializer(data=parent_token_info)
+    if (parent_serializer.is_valid()):
+        parent_serializer.save()
+    # change this to include the else case
+    return parent_token_info
 
 @api_view(["GET"])
 def confirm_enrollment(request, pk):
@@ -60,6 +67,7 @@ def cancel_enrollment(request, pk):
     enrollment_we_want.delete()
     return Response(EnrollmentSerializer(enrollment_we_want).data, status=status.HTTP_206_PARTIAL_CONTENT)
 
+
 @api_view(["POST"])
 def create_enrollment(request):
     enrollment_info = dict()
@@ -73,14 +81,15 @@ def create_enrollment(request):
         child = children_that_match.first()
         # parent_email = child.parent.email
         parent_email = request.data['parent_email']
-        activity_id = request.data["activities"][0]
+        activity_id = request.data["activity"]
         activity = Activity.objects.get(pk=activity_id)
         enrollment_info['activity'] = activity_id
         enrollment_info['child'] = child.id
         serializer = EnrollmentSaveSerializer(data=enrollment_info)
         if (serializer.is_valid()):
             e = serializer.save()
-            num_emails_sent = send_email(enrollment_id=e.id, parent_email=parent_email, child=child, activity=activity)
+            token_info = generate_token(e.id)
+            print(send_email(token=token_info['token'], parent_email=parent_email, child=child, activity=activity))
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -135,20 +144,135 @@ def send_cancellation_email(enrollment_id=None):
     return send_mail(subject, message, from_email, recipient_list, fail_silently=False, html_message=html_message)
 
 
-def send_email(enrollment_id=0, parent_email="", child="", activity="",hostname="https://banana-tart-91724.herokuapp.com/"):
-    # confirm_route = "http://injuredroman.github.io/kodiak_sign_up/#/confirm_enrollment/%d"%(enrollment_id)
-    # hostname="http://127.0.0.1:8000/" #for local testing only
-    confirm_route = hostname + "api/confirm_enrollment/{}/".format(enrollment_id)
-    cancel_route = hostname + "api/cancel_enrollment/{}/".format(enrollment_id)
-    # cancel_route = "http://injuredroman.github.io/kodiak_sign_up/#/cancel_enrollment/%d"%(enrollment_id)
+def send_email(token="", parent_email="", child="", activity=""):
+    confirm_route = "http://injuredroman.github.io/kodiak_sign_up/#/confirm_enrollment/%s"%(token)
+    cancel_route = "http://injuredroman.github.io/kodiak_sign_up/#/cancel_enrollment/%s"%(token)
     subject = "Confirm %s's enrollment in %s"%(child.first_name, activity.title)
     from_email = settings.DEFAULT_FROM_EMAIL
     message = ''
     recipient_list=[parent_email]
-    html_message = """<body> Please confirm or cancel {}'s enrollment in {}, from {} to {}, between {} and {}.\n
-          <button class="btn btn-success" "><a href="{}"> Confirm Enrollment</a></button>
-          <button class="btn btn-success" "><a href="{}"> Cancel Enrollment</a></button>
-       </body>""".format(child.first_name, activity.title, activity.start_date, activity.end_date, activity.start_time, activity.end_time, confirm_route, cancel_route)
+    html_message = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width"/>
+
+    <!-- For development, pass document through inliner -->
+
+    <style type="text/css">
+    * {{ margin: 0; padding: 0; font-size: 100%; font-family: 'Avenir Next', "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif; line-height: 1.65; }}
+
+img {{ max-width: 100%; margin: 0 auto; display: block; }}
+
+body, .body-wrap {{ width: 100% !important; height: 100%; background: #f8f8f8; }}
+
+a {{ color: #71bc37; text-decoration: none; }}
+
+a:hover {{ text-decoration: underline; }}
+
+.text-center {{ text-align: center; }}
+
+.text-right {{ text-align: right; }}
+
+.text-left {{ text-align: left; }}
+
+.button {{ display: inline-block; color: white; background: #71bc37; border: solid #71bc37; border-width: 10px 20px 8px; font-weight: bold; border-radius: 4px; }}
+
+.button:hover {{ text-decoration: none; }}
+
+h1, h2, h3, h4, h5, h6 {{ margin-bottom: 20px; line-height: 1.25; }}
+
+h1 {{ font-size: 32px; }}
+
+h2 {{ font-size: 28px; }}
+
+h3 {{ font-size: 24px; }}
+
+h4 {{ font-size: 20px; }}
+
+h5 {{ font-size: 16px; }}
+
+p, ul, ol {{ font-size: 16px; font-weight: normal; margin-bottom: 20px; }}
+
+.container {{ display: block !important; clear: both !important; margin: 0 auto !important; max-width: 580px !important; }}
+
+.container table {{ width: 100% !important; border-collapse: collapse; }}
+
+.container .masthead {{ padding: 80px 0; background: #71bc37; color: white; }}
+
+.container .masthead h1 {{ margin: 0 auto !important; max-width: 90%; text-transform: uppercase; }}
+
+.container .content {{ background: white; padding: 30px 35px; }}
+
+.container .content.footer {{ background: none; }}
+
+.container .content.footer p {{ margin-bottom: 0; color: #888; text-align: center; font-size: 14px; }}
+
+.container .content.footer a {{ color: #888; text-decoration: none; font-weight: bold; }}
+
+.container .content.footer a:hover {{ text-decoration: underline; }}
+
+    </style>
+</head>
+<body>
+<table class="body-wrap">
+    <tr>
+        <td class="container">
+
+            <!-- Message start -->
+            <table>
+                <tr>
+                    <td align="center" class="masthead">
+
+                        <h1>Confirm your child's enrollment</h1>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td class="content">
+
+                        <h2>Hi Parent,</h2>
+
+                        <p>Please confirm or cancel {}'s enrollment in {}, from {} to {}, between {} and {}.</p>
+
+                        <table>
+                            <tr>
+                                <td align="center">
+                                    <p>
+                                        <a href= {}" class="button">Confirm Enrollment</a>
+                                    </p>
+                                </td>
+                            </tr>
+                        </table>
+
+                        <p>By the way, if you're wondering where you can find more of this fine meaty filler, visit <a href="http://baconipsum.com">Bacon Ipsum</a>.</p>
+
+                        <p><em>– KIBSD</em></p>
+
+                    </td>
+                </tr>
+            </table>
+
+        </td>
+    </tr>
+    <tr>
+        <td class="container">
+
+            <!-- Message start -->
+            <table>
+                <tr>
+                    <td class="content footer" align="center">
+                        <p>Sent by <a href="#">KIBSD</a>, Address Placeholder</p>
+                        <p><a href="mailto:">hello@company.com</a> | <a href="#">Unsubscribe</a></p>
+                    </td>
+                </tr>
+            </table>
+
+        </td>
+    </tr>
+</table>
+</body>
+</html>""".format(child.first_name, activity.title, activity.start_date, activity.end_date, activity.start_time, activity.end_time, confirm_route)
     return send_mail(subject, message, from_email, recipient_list, fail_silently=False, html_message=html_message)
 
 
@@ -159,7 +283,7 @@ def activity_post(request):
             #to its running
         result_days_of_occurrence = []
         for day in request.data["days_of_occurrence"]:
-            daySerializer=DaySerializer(data={"day": day})
+            daySerializer=DaySerializer(data={{"day": day}})
             if (daySerializer.is_valid()):
                 d = daySerializer.save()
                 result_days_of_occurrence.append(d.id)
