c5fc686955ed8901f3b9f001869323db0ed46783
Jeremy Xue <jeremyxue@gmail.com>
2019-05-06 16:03:38 -0400

    no children found code

--- backend/views.py
+++ backend/views.py
@@ -125,6 +125,138 @@ def create_enrollment(request):
     children_that_match = Child.objects.filter(first_name__contains=child_fname,last_name__contains=child_lname)
     if (len(children_that_match) == 0):
         #we didn't find a child with this name, we'd like to find one
+        subject = "[KIBSD] Could not find a child with name {} {}".format(child_fname, child_lname)
+        from_email = settings.DEFAULT_FROM_EMAIL
+        html_message = html_message = """
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width"/>
+
+    <!-- For development, pass document through inliner -->
+
+    <style type="text/css">
+    * {{ margin: 0; padding: 0; font-size: 100%; font-family: 'Avenir Next', "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif; line-height: 1.65; }}
+
+    img {{ max-width: 100%; margin: 0 auto; display: block; }}
+
+    body, .body-wrap {{ width: 100% !important; height: 100%; background: #f8f8f8; }}
+
+    a {{ color: #71bc37; text-decoration: none; }}
+
+    a:hover {{ text-decoration: underline; }}
+
+    .text-center {{ text-align: center; }}
+
+    .text-right {{ text-align: right; }}
+
+    .text-left {{ text-align: left; }}
+
+    .button {{ display: inline-block; color: white; background: #71bc37; border: solid #71bc37; border-width: 10px 20px 8px; font-weight: bold; border-radius: 4px; }}
+
+    .button:hover {{ text-decoration: none; }}
+
+    h1, h2, h3, h4, h5, h6 {{ margin-bottom: 20px; line-height: 1.25; }}
+
+    h1 {{ font-size: 32px; }}
+
+    h2 {{ font-size: 28px; }}
+
+    h3 {{ font-size: 24px; }}
+
+    h4 {{ font-size: 20px; }}
+
+    h5 {{ font-size: 16px; }}
+
+    p, ul, ol {{ font-size: 16px; font-weight: normal; margin-bottom: 20px; }}
+
+    .container {{ display: block !important; clear: both !important; margin: 0 auto !important; max-width: 580px !important; }}
+
+    .container table {{ width: 100% !important; border-collapse: collapse; }}
+
+    .container .masthead {{ padding: 80px 0; background: #71bc37; color: white; }}
+
+    .container .masthead h1 {{ margin: 0 auto !important; max-width: 90%; text-transform: uppercase; }}
+
+    .container .content {{ background: white; padding: 30px 35px; }}
+
+    .container .content.footer {{ background: none; }}
+
+    .container .content.footer p {{ margin-bottom: 0; color: #888; text-align: center; font-size: 14px; }}
+
+    .container .content.footer a {{ color: #888; text-decoration: none; font-weight: bold; }}
+
+    .container .content.footer a:hover {{ text-decoration: underline; }}
+
+        </style>
+    </head>
+<body>
+<table class="body-wrap">
+    <tr>
+        <td class="container">
+
+            <!-- Message start -->
+            <table>
+                <tr>
+                    <td align="center" class="masthead">
+
+                        <h1>There was an error creating an enrollment for your child</h1>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td class="content">
+
+                        <h2>Hi Parent,</h2>
+
+                        <p> We could not find a child named {}, and were not able to process the attempted enrollments.
+
+                        """.format(child_fname) + """
+
+                        </p>
+
+                        <table>
+                            <tr>
+                                <td align="center">
+                                    <p>
+                                        <a href="{}" class="button"></a>
+                                    </p>
+                                </td>
+                            </tr>
+                        </table>
+
+                        <p>This link will expire in 1 day.</p>
+
+                        <p><em>– KIBSD</em></p>
+
+                    </td>
+                </tr>
+            </table>
+
+        </td>
+    </tr>
+    <tr>
+        <td class="container">
+
+            <!-- Message start -->
+            <table>
+                <tr>
+                    <td class="content footer" align="center">
+                        <p>Sent by <a href="#">KIBSD</a>, Address Placeholder</p>
+                        <p><a href="mailto:">hello@company.com</a> | <a href="#">Unsubscribe</a></p>
+                    </td>
+                </tr>
+            </table>
+
+        </td>
+    </tr>
+</table>
+</body>
+</html>"""
+
+        recipient_list=[request.data['parent_email']]
+        send_mail(subject, message, from_email, recipient_list, fail_silently=False, html_message=html_message)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else: #hopefully only one child was found
         child = children_that_match.first()
