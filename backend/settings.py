f7d325f3fbbe210fc5d5e2c8dd8121882b1a4ed7
Roman Kaufman <roman.a.kaufman@gmail.com>
2019-05-28 23:39:05 -0700

    static file changes

--- backend/settings.py
+++ backend/settings.py
@@ -34,11 +34,11 @@ ALLOWED_HOSTS = ['banana-tart-91724.herokuapp.com', '127.0.0.1']#, 'cfehome.hero
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
